4b93b7a37b0ca46f872e15eab5f82a61eaf80c8f
Matai <code@matai.info>
2025-05-10 14:27:08 -0400

    rewrite zoom mechanics

--- libs/open-seats-core/src/lib/mediators/konva.mediator.ts
+++ libs/open-seats-core/src/lib/mediators/konva.mediator.ts
@@ -2,61 +2,70 @@ import Konva from 'konva';
 import { KonvaOutput, SeatedSaveData } from '../models';
 
 export class KonvaMediator {
-	public static instance: KonvaMediator;
-	private _importedData?: SeatedSaveData;
-	private _stage: Konva.Stage;
-	private _seatLayer: Konva.Layer;
+  public static instance: KonvaMediator;
+  private _stage: Konva.Stage;
+  private _seatLayer!: Konva.Layer;
 
-	constructor(private _container: HTMLDivElement) {
-		KonvaMediator.instance = this;
-		this._stage = new Konva.Stage({
-			container: _container, // id of container <div>
-			width: _container.getBoundingClientRect().width,
-			height: _container.getBoundingClientRect().height,
-		});
-		this._seatLayer = new Konva.Layer({ name: 'seatLayer', draggable: false });
-		this._stage.add(this._seatLayer);
-		this._stage.container().style.backgroundColor = '#333333'; // Change background color
-		this._seatLayer.draw();
+  constructor(private _container: HTMLDivElement) {
+    KonvaMediator.instance = this;
+    this._stage = this.createStage(
+      _container.getBoundingClientRect().width,
+      _container.getBoundingClientRect().height,
+      _container
+    );
+  }
 
-		window.addEventListener('resize', () => {
-			this._scaleChange();
-		});
-	}
+  public createStage(
+    x: number,
+    y: number,
+    container: HTMLDivElement
+  ): Konva.Stage {
+    const stage = new Konva.Stage({
+      container: container, // id of container <div>
+      width: x,
+      height: y,
+    });
+    this._seatLayer = new Konva.Layer({ name: 'seatLayer', draggable: false });
+    stage.add(this._seatLayer);
+    this._seatLayer.draw();
+    stage.on('wheel', this._handleScroll.bind(this));
+    return stage;
+  }
 
-	private _scaleChange(): void {
-		if (!this._importedData) return;
-		const containerWidth = this._container.offsetWidth;
-		const sceneWidth = this._importedData.dimentions.width;
-		const sceneHeight = this._importedData.dimentions.height;
-		const scale = containerWidth / sceneWidth;
-		this._stage.width(sceneWidth * scale);
-		this._stage.height(sceneHeight * scale);
-		this._stage.scale({ x: scale, y: scale });
-	}
+  public addToSeatLayer(children: Konva.Shape | Konva.Group) {
+    this._seatLayer.add(children);
+  }
 
-	public createStage(x: number, y: number, container: HTMLDivElement): void {
-		this._stage = new Konva.Stage({
-			container: container, // id of container <div>
-			width: x,
-			height: y,
-		});
-		this._seatLayer = new Konva.Layer({ name: 'seatLayer' });
-		this._stage.add(this._seatLayer);
-		this._seatLayer.draw();
-	}
+  public import(data: SeatedSaveData): void {
+    this._seatLayer.clear();
+    this._stage.destroyChildren();
+    this._stage.clear();
+    this._stage = this.createStage(
+      data.dimentions.width,
+      data.dimentions.height,
+      this._container
+    );
+  }
 
-	public addToSeatLayer(children: Konva.Shape | Konva.Group) {
-		this._seatLayer.add(children);
-	}
+  public export(): KonvaOutput {
+    return this._stage.toObject();
+  }
 
-	public import(data: SeatedSaveData): void {
-		this._importedData = data;
-		this.createStage(data.dimentions.width, data.dimentions.height, this._container);
-		this._seatLayer.clear();
-	}
-
-	public export(): KonvaOutput {
-		return this._stage.toObject();
-	}
+  private _handleScroll(e: Konva.KonvaEventObject<WheelEvent>) {
+    e.evt.preventDefault();
+    const scaleBy = 1.1;
+    const oldScale = this._stage.scaleX();
+    const mousePointTo = {
+      x: (e.evt.clientX - this._stage.x()) / oldScale,
+      y: (e.evt.clientY - this._stage.y()) / oldScale,
+    };
+    const newScale = e.evt.deltaY > 0 ? oldScale * scaleBy : oldScale / scaleBy;
+    this._stage.scale({ x: newScale, y: newScale });
+    const newPos = {
+      x: e.evt.clientX - mousePointTo.x * newScale,
+      y: e.evt.clientY - mousePointTo.y * newScale,
+    };
+    this._stage.position(newPos);
+    this._stage.batchDraw();
+  }
 }
