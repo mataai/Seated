b5bc9f9d4534da2abb70d37791cfef920cd48861
Matai <code@matai.info>
2025-05-10 12:37:44 -0400

    import angular

--- apps/open-seats-demo/eslint.config.mjs
+++ apps/open-seats-demo/eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
         'error',
         {
           type: 'attribute',
-          prefix: 'app',
+          prefix: 'open-seats',
           style: 'camelCase',
         },
       ],
@@ -20,7 +20,7 @@ export default [
         'error',
         {
           type: 'element',
-          prefix: 'app',
+          prefix: 'open-seats',
           style: 'kebab-case',
         },
       ],
--- apps/open-seats-demo/src/app/app.component.ts
+++ apps/open-seats-demo/src/app/app.component.ts
@@ -1,13 +1,51 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Seated } from '@open-seats/core';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+	standalone: true,
+	selector: 'open-seats-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'open-seats-demo';
+export class AppComponent implements AfterViewInit {
+	@ViewChild('chartContainer') chartContainer!: ElementRef;
+
+	title = 'angular';
+	chart!: Seated;
+
+	ngAfterViewInit() {
+		console.log('this.chartContainer', this.chartContainer);
+		this.chart = new Seated(this.chartContainer.nativeElement, true);
+	}
+
+	public addChair(): void {
+		this.chart.createSeat({
+			x: 100,
+			y: 100,
+			radius: 20,
+			color: '#000000',
+			id: 'chair',
+			data: {},
+		});
+	}
+
+	public addTable(): void {
+		this.chart.createTable({
+			x: 100,
+			y: 100,
+			width: 100,
+			height: 100,
+			rotation: 0,
+			color: '#000000',
+			id: 'table',
+			data: {},
+		});
+	}
+
+	public save(): void {
+		localStorage.setItem('seated', JSON.stringify(this.chart.export()));
+	}
+	public load(): void {
+		this.chart.import(JSON.parse(localStorage.getItem('seated') || ''));
+	}
 }
